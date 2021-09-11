2b022be593387cb746471726c6f0c997b8ec9148
EdsonMacaule <edsonmacaule92@gmail.com>
2021-09-11 12:43:22 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,271 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <title>Meu Curriculum</title>
+    <link rel="icon" type="image/x-icon" href="assets/img/favicon.ico" />
+    <!-- Font Awesome icons (free version)-->
+    <script src="https://use.fontawesome.com/releases/v5.15.3/js/all.js" crossorigin="anonymous"></script>
+    <!-- Google fonts-->
+    <link href="https://fonts.googleapis.com/css?family=Saira+Extra+Condensed:500,700" rel="stylesheet"
+        type="text/css" />
+    <link href="https://fonts.googleapis.com/css?family=Muli:400,400i,800,800i" rel="stylesheet" type="text/css" />
+    <!-- Core theme CSS (includes Bootstrap)-->
+    <link href="css/styles.css" rel="stylesheet" />
+</head>
+
+<body id="page-top">
+    <!-- Navigation-->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="sideNav">
+        <a class="navbar-brand js-scroll-trigger" href="#page-top">
+            <span class="d-block d-lg-none">Edson Macaule</span>
+            <span class="d-none d-lg-block"><img class="img-fluid img-profile rounded-circle mx-auto mb-2"
+                    src="assets/img/macaule.jpg" alt="..." /></span>
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive"
+            aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span
+                class="navbar-toggler-icon"></span></button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+            <ul class="navbar-nav">
+                <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#about">Soubre Mim</a></li>
+                <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#experience">Experiência</a></li>
+                <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#education">Educação</a></li>
+                <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#skills">Competências</a></li>
+                <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#interests">Interesses</a></li>
+                <!--<li class="nav-item"><a class="nav-link js-scroll-trigger" href="#awards">Certificação</a></li>-->
+            </ul>
+        </div>
+    </nav>
+    <!-- Page Content-->
+    <div class="container-fluid p-0">
+        <!-- About-->
+        <section class="resume-section" id="about">
+            <div class="resume-section-content">
+                <h1 class="mb-0">
+                    Edson
+                    <span class="text-primary">Macaule</span>
+                </h1>
+                <div class="subheading mb-5">
+                    Matola · Moçambique · (+258) 84/87 757 3571 ·
+                    <a href="mailto:edsonmacaule92@gmail.com">edsonmacaule92@gmail.com</a>
+                </div>
+                <p class="lead mb-5">Tenho experiência em utilizar metodologias ágeis para fornecer uma solução robusta
+                    e de alto nível. </p>
+                <div class="social-icons">
+                    <a class="social-icon" href="https://www.linkedin.com/in/edson-macaule-625491183/"><i
+                            class="fab fa-linkedin-in"></i></a>
+                    <a class="social-icon" href="https://github.com/EdsonMacaule"><i class="fab fa-github"></i></a>
+                    <a class="social-icon" href="https://twitter.com/Macaule8"><i class="fab fa-twitter"></i></a>
+                    <a class="social-icon" href="https://www.facebook.com/edsonanselmo.macaule1"><i
+                            class="fab fa-facebook-f"></i></a>
+                    <!--<a class="social-icon" href="https://whats.link/edsonmacaule"><i class="fab fa-whatsapp"></i></a>-->
+                </div>
+            </div>
+        </section>
+        <hr class="m-0" />
+        <!-- Experience-->
+        <section class="resume-section" id="experience">
+            <div class="resume-section-content">
+                <h2 class="mb-5">Experiência</h2>
+                <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                    <div class="flex-grow-1">
+                        <h3 class="mb-0">Director de TI</h3>
+                        <div class="subheading mb-3">Agência Criativa</div>
+                        <p>Desenvolvimento web, gestão de clientes e de projectos, coordenação de actividades
+                            planificação de actividades.
+                        </p>
+                    </div>
+                    <div class="flex-shrink-0"><span class="text-primary">Novembro 2020 - Presente</span></div>
+                </div>
+                <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                    <div class="flex-grow-1">
+                        <h3 class="mb-0">Técnico informático</h3>
+                        <div class="subheading mb-3">Freelancer</div>
+                        <p>Desenvolvimento e Administração de Websites empresárias e pessoais,
+                            Instalação de redes Estruturadas a cabo e Wi-Fi, Helpdesk, Manutenção Preventiva e
+                            Correctiva de Hardware e
+                            Software.</p>
+                    </div>
+                    <div class="flex-shrink-0"><span class="text-primary">Solicitação</span></div>
+                </div>
+                <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                    <div class="flex-grow-1">
+                        <h3 class="mb-0">Gerente Administrativo</h3>
+                        <div class="subheading mb-3">SITIO DA CHANTEL</div>
+                        <p>Gerir, Administrar, Procurement e coordenar diversas actividades da área.</p>
+                    </div>
+                    <div class="flex-shrink-0"><span class="text-primary">Por 6 Meses</span></div>
+                </div>
+                <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                    <div class="flex-grow-1">
+                        <h3 class="mb-0">Técnico informático</h3>
+                        <div class="subheading mb-3">SIKA GROUP</div>
+                        <p>Gerenciamento de rede de computador, Helpdesk, manutenção de Hardware e Software,
+                            Administração de Servidor.</p>
+                    </div>
+                    <div class="flex-shrink-0"><span class="text-primary">Por 2 Anos</span></div>
+                </div>
+                <div class="d-flex flex-column flex-md-row justify-content-between">
+                    <div class="flex-grow-1">
+                        <h3 class="mb-0">Formador</h3>
+                        <div class="subheading mb-3">Matola Communication Center</div>
+                        <p>Formar, Capacitar e ensinar o uso das TICS.</p>
+                    </div>
+                    <div class="flex-shrink-0"><span class="text-primary">Por 1 Ano</span></div>
+                </div>
+            </div>
+        </section>
+        <hr class="m-0" />
+        <!-- Education-->
+        <section class="resume-section" id="education">
+            <div class="resume-section-content">
+                <h2 class="mb-5">Educação</h2>
+                <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                    <div class="flex-grow-1">
+                        <h3 class="mb-0">Instituito de Ciências da Computação e Administração (ICCA)</h3>
+                        <div class="subheading mb-3">Tecnico Profissional</div>
+                        <div>Sistemas Informaticos - Programador</div>
+                        <p>Media Final: 14</p>
+                    </div>
+                    <div class="flex-shrink-0"><span class="text-primary">Janeiro 2017 - Outubro 2018</span></div>
+                </div>
+                <div class="d-flex flex-column flex-md-row justify-content-between">
+                    <div class="flex-grow-1">
+                        <h3 class="mb-0">Escola Secundária Joaquim Chissano</h3>
+                        <div class="subheading mb-3">Ciências com Biologia</div>
+                        <p>Media Final: 14</p>
+                    </div>
+                    <div class="flex-shrink-0"><span class="text-primary">2014</span></div>
+                </div>
+            </div>
+        </section>
+        <hr class="m-0" />
+        <!-- Skills-->
+        <section class="resume-section" id="skills">
+            <div class="resume-section-content">
+                <h2 class="mb-5">Competências</h2>
+                <div class="subheading mb-3">Linguagens de Programação & Frameworks</div>
+                <ul class="list-inline dev-icons">
+                    <li class="list-inline-item"><i class="fab fa-html5"></i></li>
+                    <li class="list-inline-item"><i class="fab fa-css3-alt"></i></li>
+                    <li class="list-inline-item"><i class="fab fa-php"></i></li>
+                    <li class="list-inline-item"><i class="fab fa-java"></i></li>
+                    <li class="list-inline-item"><i class="fab fa-bootstrap"></i></li>
+                    <li class="list-inline-item"><i class="fab fa-elementor"></i></li>
+                    <li class="list-inline-item"><i class="fab fa-wordpress"></i></li>
+                    <li class="list-inline-item"><i class="fab fa-joomla"></i></li>
+                </ul>
+                <div class="subheading mb-3">Outras Áreas de Conhecimento</div>
+                <ul class="fa-ul mb-0">
+                    <li>
+                        <span class="fa-li"><i class="fas fa-check"></i></span>
+                        Desenho e implementação de bases de dados
+                    </li>
+                    <li>
+                        <span class="fa-li"><i class="fas fa-check"></i></span>
+                        Gestão de uma Base de Dados
+                    </li>
+                    <li>
+                        <span class="fa-li"><i class="fas fa-check"></i></span>
+                        Hardware e Software de Computadores
+                    </li>
+                    <li>
+                        <span class="fa-li"><i class="fas fa-check"></i></span>
+                        Auditoria de Sistemas
+                    </li>
+                    <li>
+                        <span class="fa-li"><i class="fas fa-check"></i></span>
+                        Administração de Sistemas (Windows, iOS)
+                    </li>
+                    <li>
+                        <span class="fa-li"><i class="fas fa-check"></i></span>
+                        Domínio do pacote Ms – Office
+                    </li>
+                    <li>
+                        <span class="fa-li"><i class="fas fa-check"></i></span>
+                        Designer Gráfico (CorelDRAW, Ilustrator, Photoshop)
+                    </li>
+                    <li>
+                        <span class="fa-li"><i class="fas fa-check"></i></span>
+                        Domínio de Windows Server
+                    </li>
+                    <li>
+                        <span class="fa-li"><i class="fas fa-check"></i></span>
+                        Redes de Computador (Cablagem, Configuração, Manutenção e Administração de Redes,
+                        configuração de Roteador)
+                    </li>
+                    <li>
+                        <span class="fa-li"><i class="fas fa-check"></i></span>
+                        Marketing digital
+                    </li>
+                </ul>
+            </div>
+        </section>
+        <hr class="m-0" />
+        <!-- Interests-->
+        <section class="resume-section" id="interests">
+            <div class="resume-section-content">
+                <h2 class="mb-5">Interesses</h2>
+                <p>Futebol, Filmes de Guerra, Seriados, Aventuras, Teledramaturgia.</p>
+                <p class="mb-0">Leitura, Meetups, Política, Boa Comida.</p>
+            </div>
+        </section>
+        <hr class="m-0" />
+        <!-- Awards-->
+        <!-- <section class="resume-section" id="awards">
+            <div class="resume-section-content">
+                <h2 class="mb-5">Awards & Certifications</h2>
+                <ul class="fa-ul mb-0">
+                    <li>
+                        <span class="fa-li"><i class="fas fa-trophy text-warning"></i></span>
+                        Google Analytics Certified Developer
+                    </li>
+                    <li>
+                        <span class="fa-li"><i class="fas fa-trophy text-warning"></i></span>
+                        Mobile Web Specialist - Google Certification
+                    </li>
+                    <li>
+                        <span class="fa-li"><i class="fas fa-trophy text-warning"></i></span>
+                        1
+                        <sup>st</sup>
+                        Place - University of Colorado Boulder - Emerging Tech Competition 2009
+                    </li>
+                    <li>
+                        <span class="fa-li"><i class="fas fa-trophy text-warning"></i></span>
+                        1
+                        <sup>st</sup>
+                        Place - University of Colorado Boulder - Adobe Creative Jam 2008 (UI Design Category)
+                    </li>
+                    <li>
+                        <span class="fa-li"><i class="fas fa-trophy text-warning"></i></span>
+                        2
+                        <sup>nd</sup>
+                        Place - University of Colorado Boulder - Emerging Tech Competition 2008
+                    </li>
+                    <li>
+                        <span class="fa-li"><i class="fas fa-trophy text-warning"></i></span>
+                        1
+                        <sup>st</sup>
+                        Place - James Buchanan High School - Hackathon 2006
+                    </li>
+                    <li>
+                        <span class="fa-li"><i class="fas fa-trophy text-warning"></i></span>
+                        3
+                        <sup>rd</sup>
+                        Place - James Buchanan High School - Hackathon 2005
+                    </li>
+                </ul>
+            </div>
+        </section> -->
+    </div>
+    <!-- Bootstrap core JS-->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
+    <!-- Core theme JS-->
+    <script src="js/scripts.js"></script>
+</body>
+
+</html>
