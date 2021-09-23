ec6f95cc2ce064338c226b0d68a7e6a5e753a6ab
EdsonMacaule <edsonmacaule92@gmail.com>
2021-09-23 15:30:17 +0200

    versao 1.2

--- index.php
+++ index.php
@@ -62,7 +62,7 @@
                     <a class="social-icon" href="https://twitter.com/Macaule8"><i class="fab fa-twitter"></i></a>
                     <a class="social-icon" href="https://www.facebook.com/edsonanselmo.macaule1"><i
                             class="fab fa-facebook-f"></i></a>
-                    <!--<a class="social-icon" href="https://whats.link/edsonmacaule"><i class="fab fa-whatsapp"></i></a>-->
+                    <a class="social-icon" href="https://wa.link/vd6z7y"><i class="fab fa-whatsapp"></i></a>
                 </div>
             </div>
         </section>
@@ -75,8 +75,8 @@
                     <div class="flex-grow-1">
                         <h3 class="mb-0">Director de TI</h3>
                         <div class="subheading mb-3">Agência Criativa</div>
-                        <p>Desenvolvimento web, gestão de clientes e de projectos, coordenação de actividades
-                            planificação de actividades.
+                        <p>Desenvolvimento web, gestão de clientes e de projectos, coordenação e planificação de
+                            actividades...
                         </p>
                     </div>
                     <div class="flex-shrink-0"><span class="text-primary">Novembro 2020 - Presente</span></div>
@@ -88,7 +88,7 @@
                         <p>Desenvolvimento e Administração de Websites empresárias e pessoais,
                             Instalação de redes Estruturadas a cabo e Wi-Fi, Helpdesk, Manutenção Preventiva e
                             Correctiva de Hardware e
-                            Software.</p>
+                            Software...</p>
                     </div>
                     <div class="flex-shrink-0"><span class="text-primary">Solicitação</span></div>
                 </div>
@@ -96,7 +96,7 @@
                     <div class="flex-grow-1">
                         <h3 class="mb-0">Gerente Administrativo</h3>
                         <div class="subheading mb-3">SITIO DA CHANTEL</div>
-                        <p>Gerir, Administrar, Procurement e coordenar diversas actividades da área.</p>
+                        <p>Gerir, Administrar, Procurement e coordenar diversas actividades da área...</p>
                     </div>
                     <div class="flex-shrink-0"><span class="text-primary">Por 6 Meses</span></div>
                 </div>
@@ -105,7 +105,7 @@
                         <h3 class="mb-0">Técnico informático</h3>
                         <div class="subheading mb-3">SIKA GROUP</div>
                         <p>Gerenciamento de rede de computador, Helpdesk, manutenção de Hardware e Software,
-                            Administração de Servidor.</p>
+                            Administração de Servidor...</p>
                     </div>
                     <div class="flex-shrink-0"><span class="text-primary">Por 2 Anos</span></div>
                 </div>
@@ -113,7 +113,7 @@
                     <div class="flex-grow-1">
                         <h3 class="mb-0">Formador</h3>
                         <div class="subheading mb-3">Matola Communication Center</div>
-                        <p>Formar, Capacitar e ensinar o uso das TICS.</p>
+                        <p>Formar, Capacitar e ensinar o uso das TICS...</p>
                     </div>
                     <div class="flex-shrink-0"><span class="text-primary">Por 1 Ano</span></div>
                 </div>
@@ -148,7 +148,7 @@
         <section class="resume-section" id="skills">
             <div class="resume-section-content">
                 <h2 class="mb-5">Competências</h2>
-                <div class="subheading mb-3">Linguagens de Programação & Frameworks</div>
+                <div class="subheading mb-3">Linguagens de Programação, Frameworks & Ferramentas de Design</div>
                 <ul class="list-inline dev-icons">
                     <li class="list-inline-item"><i class="fab fa-html5"></i></li>
                     <li class="list-inline-item"><i class="fab fa-css3-alt"></i></li>
@@ -158,6 +158,77 @@
                     <li class="list-inline-item"><i class="fab fa-elementor"></i></li>
                     <li class="list-inline-item"><i class="fab fa-wordpress"></i></li>
                     <li class="list-inline-item"><i class="fab fa-joomla"></i></li>
+                    <li class="list-inline-item"><svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="50"
+                            height="50" viewBox="0 0 172 172" style=" fill:#000000;">
+                            <g fill="none" fill-rule="nonzero" stroke="none" stroke-width="1" stroke-linecap="butt"
+                                stroke-linejoin="miter" stroke-miterlimit="10" stroke-dasharray="" stroke-dashoffset="0"
+                                font-family="none" font-weight="none" font-size="none" text-anchor="none"
+                                style="mix-blend-mode: normal">
+                                <path d="M0,172v-172h172v172z" fill="none"></path>
+                                <g fill="#666666">
+                                    <path
+                                        d="M110.92624,76.06528c-7.04512,0 -9.57352,9.1504 -9.57352,19.82472c0,11.22816 2.75544,21.97816 8.91992,21.97816c2.64192,0 6.05784,-1.376 7.9292,-9.07816c0.33024,-0.99072 0.43,-3.01 0.43,-3.01v-15.69672c0,-1.09736 0.11008,-2.44584 0,-3.65328c-0.44032,-5.504 -3.63264,-10.36472 -7.7056,-10.36472zM151.36,13.76h-130.72c-3.79432,0 -6.88,3.08568 -6.88,6.88v130.72c0,3.79432 3.08568,6.88 6.88,6.88h130.72c3.79432,0 6.88,-3.08568 6.88,-6.88v-130.72c0,-3.79432 -3.08568,-6.88 -6.88,-6.88zM91.16,123.84h-8.6l-14.59592,-27.95688l-14.64408,27.95688h-8.47616l18.85808,-36.12l-18.85808,-36.12h8.47616l14.64408,27.95688l14.59592,-27.95688h8.6l-18.92,36.12zM127.28,112.23c0,3.63264 -0.11008,8.52776 0,11.61h-8.6v-5.59c-1.6512,3.74272 -4.77472,6.66328 -10.38536,6.66328c-9.02656,0 -15.41464,-11.40704 -15.41464,-29.02328c0,-18.93032 7.92232,-27.09 16.17832,-27.09c3.8528,0 7.5336,1.94016 9.62168,6.45v-0.02408v-27.06592h8.6z">
+                                    </path>
+                                </g>
+                            </g>
+                        </svg>
+                    </li>
+                    <li class="list-inline-item"><svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="50"
+                            height="50" viewBox="0 0 172 172" style=" fill:#000000;">
+                            <g fill="none" fill-rule="nonzero" stroke="none" stroke-width="1" stroke-linecap="butt"
+                                stroke-linejoin="miter" stroke-miterlimit="10" stroke-dasharray="" stroke-dashoffset="0"
+                                font-family="none" font-weight="none" font-size="none" text-anchor="none"
+                                style="mix-blend-mode: normal">
+                                <path d="M0,172v-172h172v172z" fill="none"></path>
+                                <g fill="#666666">
+                                    <path
+                                        d="M20.64,13.76c-3.784,0 -6.88,3.096 -6.88,6.88v130.72c0,3.784 3.096,6.88 6.88,6.88h130.72c3.784,0 6.88,-3.096 6.88,-6.88v-130.72c0,-3.784 -3.096,-6.88 -6.88,-6.88zM68.55813,48.16h9.35922l16.50797,75.68h-8.39172l-4.78375,-22.11813h-16.50797l-4.88453,22.11813h-8.25735zM112.59281,48.16c3.1992,0 5.22719,2.16908 5.22719,5.36828c0,3.2336 -2.02799,5.40187 -5.22719,5.40187h-0.1075c-2.8208,0 -4.98531,-2.16827 -4.98531,-5.40187c0,-3.1992 2.27201,-5.36828 5.09281,-5.36828zM73.03281,56.55172c-0.8944,5.6072 -1.99655,12.00883 -3.09735,17.06563l-3.78265,19.26265h14.06906l-3.89015,-19.37015c-1.1008,-5.2976 -2.09545,-11.45413 -2.98985,-16.95813zM108.36,65.36h8.6v58.48h-8.6z">
+                                    </path>
+                                </g>
+                            </g>
+                        </svg></li>
+                    <li class="list-inline-item"><svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="50"
+                            height="50" viewBox="0 0 172 172" style=" fill:#000000;">
+                            <g fill="none" fill-rule="nonzero" stroke="none" stroke-width="1" stroke-linecap="butt"
+                                stroke-linejoin="miter" stroke-miterlimit="10" stroke-dasharray="" stroke-dashoffset="0"
+                                font-family="none" font-weight="none" font-size="none" text-anchor="none"
+                                style="mix-blend-mode: normal">
+                                <path d="M0,172v-172h172v172z" fill="none"></path>
+                                <g fill="#666666">
+                                    <path
+                                        d="M151.36,13.76h-130.72c-3.784,0 -6.88,3.096 -6.88,6.88v130.72c0,3.784 3.096,6.88 6.88,6.88h130.72c3.784,0 6.88,-3.096 6.88,-6.88v-130.72c0,-3.784 -3.096,-6.88 -6.88,-6.88zM86,123.84h-30.96v-75.68h8.6v67.08h22.36zM116.96,76.0928c-0.5504,-0.1032 -1.1008,-0.1032 -1.7544,-0.1032c-5.3664,0 -8.9784,5.0224 -9.8728,11.18c-0.2064,1.4448 -0.344,2.9928 -0.344,4.8504l-0.0688,31.82h-8.6c0,0 0.1032,-51.084 0,-55.04h8.6v7.74l0.4128,-1.2728c1.7544,-5.0912 4.9192,-7.3272 9.8728,-7.3272c0.6536,0 1.204,0 1.7544,0.1032z">
+                                    </path>
+                                </g>
+                            </g>
+                        </svg></li>
+                    <li class="list-inline-item"><svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="50"
+                            height="50" viewBox="0 0 172 172" style=" fill:#000000;">
+                            <g fill="none" fill-rule="nonzero" stroke="none" stroke-width="1" stroke-linecap="butt"
+                                stroke-linejoin="miter" stroke-miterlimit="10" stroke-dasharray="" stroke-dashoffset="0"
+                                font-family="none" font-weight="none" font-size="none" text-anchor="none"
+                                style="mix-blend-mode: normal">
+                                <path d="M0,172v-172h172v172z" fill="none"></path>
+                                <g fill="#666666">
+                                    <path
+                                        d="M20.64,13.76c-3.784,0 -6.88,3.096 -6.88,6.88v130.72c0,3.784 3.096,6.88 6.88,6.88h130.72c3.784,0 6.88,-3.096 6.88,-6.88v-130.72c0,-3.784 -3.096,-6.88 -6.88,-6.88zM63.91547,48.16c6.8456,0 12.66108,1.65228 16.54828,5.40187c3.5088,3.5088 5.36156,8.91121 5.36156,15.41281c0,6.8112 -1.64636,11.42134 -4.50156,15.17094c-3.956,5.504 -10.90426,8.25062 -17.50906,8.25062c-1.4104,0 -2.51066,0.00242 -3.74906,-0.10078v31.54453h-8.46563v-74.23547c3.4056,-0.8944 7.36187,-1.44453 12.31547,-1.44453zM65.29281,53.80375c-1.9952,0 -3.64936,0.20694 -4.75016,0.55094v29.61625c0.9976,0.1032 1.99601,0.20828 3.40641,0.20828c8.256,0 13.86078,-5.50615 13.86078,-15.96375c0,-7.6024 -3.05703,-14.41172 -12.51703,-14.41172zM109.08563,68.8c4.3688,0 7.53145,1.34429 9.83625,2.75469l-2.30453,6.42985c-2.1672,-1.4448 -4.43895,-2.4389 -7.39735,-2.4389c-4.6784,0 -7.08828,3.29649 -7.08828,6.83969c0,3.5432 1.37681,5.64133 6.84641,9.18453c6.3984,3.9904 11.42187,8.944 11.42187,16.34c0,10.7328 -7.53225,15.93015 -16.68265,15.93015c-3.9904,0 -8.42935,-1.20535 -10.83735,-3.09735l2.17015,-6.39625c1.9608,1.204 5.36586,2.74797 8.90906,2.74797c4.9192,0 8.11625,-2.9928 8.11625,-7.74c0,-4.0936 -1.48108,-6.94477 -7.08828,-10.48797c-7.1896,-4.5408 -11.0725,-9.18372 -11.0725,-15.23812c0,-8.7376 6.60534,-14.82828 15.17094,-14.82828z">
+                                    </path>
+                                </g>
+                            </g>
+                        </svg></li>
+                    <li class="list-inline-item"><svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="50"
+                            height="50" viewBox="0 0 172 172" style=" fill:#000000;">
+                            <g fill="none" fill-rule="nonzero" stroke="none" stroke-width="1" stroke-linecap="butt"
+                                stroke-linejoin="miter" stroke-miterlimit="10" stroke-dasharray="" stroke-dashoffset="0"
+                                font-family="none" font-weight="none" font-size="none" text-anchor="none"
+                                style="mix-blend-mode: normal">
+                                <path d="M0,172v-172h172v172z" fill="none"></path>
+                                <g fill="#666666">
+                                    <path
+                                        d="M75.68,0c-24.08,0 -41.28,24.08 -41.28,48.16c0,53.32 44.72,96.32 44.72,96.32c0,0 -30.96,-44.72 -30.96,-96.32c0,-41.28 27.52,-48.16 27.52,-48.16zM86,0c-8.944,0 -24.08,10.86696 -24.08,51.6c0,38.03952 19.18972,88.53673 20.00844,90.66953l0.84656,2.21047h6.45l0.84656,-2.21047c0.81528,-2.1328 20.00844,-52.63001 20.00844,-90.66953c0,-40.73304 -15.136,-51.6 -24.08,-51.6zM96.32,0c0,0 27.52,6.88 27.52,48.16c0,51.6 -30.96,96.32 -30.96,96.32c0,0 44.72,-43 44.72,-96.32c0,-24.08 -17.2,-48.16 -41.28,-48.16zM48.16,6.88c0,0 -27.52,10.32 -27.52,41.28c0,58.48 53.32,96.32 53.32,96.32c0,0 -46.44,-41.28 -46.44,-96.32c0,-28.38 20.64,-41.28 20.64,-41.28zM123.84,6.88c0,0 20.64,12.9 20.64,41.28c0,55.04 -46.44,96.32 -46.44,96.32c0,0 53.32,-37.84 53.32,-96.32c0,-30.96 -27.52,-41.28 -27.52,-41.28zM72.24,151.36c-1.05952,0 -2.05405,0.48768 -2.70765,1.3236c-0.6536,0.83592 -0.88956,1.92097 -0.63156,2.94953l3.44,13.76c0.38528,1.52736 1.76026,2.60687 3.33922,2.60687h20.64c1.57896,0 2.95394,-1.07607 3.33922,-2.60687l3.44,-13.76c0.258,-1.02856 0.0186,-2.11361 -0.63156,-2.94953c-0.65016,-0.83592 -1.64813,-1.3236 -2.70765,-1.3236z">
+                                    </path>
+                                </g>
+                            </g>
+                        </svg></li>
                 </ul>
                 <div class="subheading mb-3">Outras Áreas de Conhecimento</div>
                 <ul class="fa-ul mb-0">
@@ -187,7 +258,7 @@
                     </li>
                     <li>
                         <span class="fa-li"><i class="fas fa-check"></i></span>
-                        Designer Gráfico (CorelDRAW, Ilustrator, Photoshop)
+                        Designer Gráfico Adoble(XD, Ilustrator, Lightroom, Photoshop, CorelDRAW)
                     </li>
                     <li>
                         <span class="fa-li"><i class="fas fa-check"></i></span>
@@ -210,8 +281,16 @@
         <section class="resume-section" id="interests">
             <div class="resume-section-content">
                 <h2 class="mb-5">Interesses</h2>
-                <p>Novas Linguagens de Programação, Desenvolvimento RPA, PowerBI</p>
-                <p class="mb-0">Inteligência Artificial, Desenvolvimento Mobile e outros</p>
+                <ul class="fa-ul mb-0">
+                    <li>
+                        <span class="fa-li"><i class="fas fa-circle"></i></span>
+                        Novas Linguagens de Programação, Desenvolvimento RPA, PowerBI
+                    </li>
+                    <li>
+                        <span class="fa-li"><i class="fas fa-circle"></i></span>
+                        Inteligência Artificial, Desenvolvimento Mobile e outros
+                    </li>
+                </ul>
             </div>
         </section>
         <hr class="m-0" />
